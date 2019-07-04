43cea413fab9afe830058367e1840346a357a9ac
Carmelo Mayen <carmelo5mayen@gmail.com>
2019-07-04 08:16:23 -0600

    Carga Inicial

---
+++ BdgTest3/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcEjemplo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "You must enter a {0}")]
+        [StringLength(30, ErrorMessage =
+            "The field {0} can contain maximun {1} and minimum {2} characters",
+            MinimumLength = 3)]
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Last Buy")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime LastBuy { get; set; }
+
+        public float Stock { get; set; }
+
+    }
+}
